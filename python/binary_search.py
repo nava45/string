0857c223f026f0b032a0295ba2f927e3ede7b2f2
Navaneethan <navaneethan.r@edgenetworks.in>
2016-11-24 06:10:32 +0530

    binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -17,8 +17,31 @@ def binary_search(slist, item):
     return found
     
 
+def recursive_binary_search(slist, item):
+    first = 0
+    last = len(slist)
+  
+    middle = (first+last) // 2
+
+    if not slist:
+        return False
+
+    if slist[middle] == item:
+        return True
+    else:
+        if slist[middle] < item:
+            return recursive_binary_search(slist[middle:], item)
+        else:
+            return recursive_binary_search(slist[:middle], item)
+    return False
+  
+
 
 if __name__ == '__main__':
     print binary_search(list(range(21)), 13)
     print binary_search([1], 1)
     print binary_search([3,4], 2)
+
+    print recursive_binary_search(list(range(21)), 13)
+    print recursive_binary_search([1], 1)
+    print recursive_binary_search([3,4], 2)
